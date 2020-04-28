99eab94834b8105e43fa795d3ae7d2b8c510cf99
Nitin Reddy <reddynitin3@gmail.com>
2020-04-28 13:00:07 +0530

    Basics of Row, Column, Center are covered

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,55 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
+class MyApp extends StatelessWidget {
+  void _clickHandler() {
+    print('Clicked');
+  }
 
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.green,
+        accentColor: Colors.orange,
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text("Flutter App"),
+          centerTitle: true,
+          backgroundColor: Colors.black,
         ),
-        body: Container(
-          child: Center(child: Text('App Body'),)
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Deliver features faster'),
+              Text('Craft beautiful UIs'),
+              RaisedButton(
+                onPressed: () => {},
+                child: Text(
+                  'Submit',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                color: Colors.purple,
+                splashColor: Colors.blue,
+              )
+            ],
+          ),
         ),
-      )
+        floatingActionButton: FloatingActionButton(
+          onPressed: _clickHandler,
+          child: Icon(
+            Icons.add_a_photo,
+            color: Colors.white,
+          ),
+        ),
+      ),
     );
   }
 }
