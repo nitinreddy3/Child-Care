dd245260126986f4a7e3ab1e5453c7efc8cd9057
Nitin Reddy <reddynitin3@gmail.com>
2020-04-29 17:21:23 +0530

    Completed the dice roller app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'custom_widgets/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,76 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BG Changer',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Random Background'),
-        ),
-        body: HomePage(),
+      title: 'Dice App',
+      theme: ThemeData(
+        primaryColor: Colors.amber,
       ),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  var colors = [
-    Colors.amber,
-    Colors.black,
-    Colors.blue,
-    Colors.red,
-    Colors.green,
-    Colors.pink,
-    Colors.orange,
-  ];
-
-  var btnColors = [
-    Colors.amberAccent,
-    Colors.brown,
-    Colors.teal,
-    Colors.green,
-    Colors.pink,
-    Colors.orange,
-  ];
-
-  var currentColor = Colors.white;
-  var currentBtnColor = Colors.black;
-
-  changeColor() {
-    var rnd = Random().nextInt(6);
-    var btnRnd = Random().nextInt(5);
-    setState(() {
-      currentColor = colors[rnd];
-      currentBtnColor = btnColors[btnRnd];
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: currentColor,
-      child: Center(
-        child: RaisedButton(
-          color: currentBtnColor,
-          child: Text(
-            'Change Color',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          onPressed: changeColor,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30.0),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
